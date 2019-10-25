b77ab6e40c04b81a412a83735ec0fd3650d46362
Renato Violin <renato@MacBook-Pro-de-Renato.local>
2019-10-25 14:39:17 -0300

    ajuste fa-1_5

--- static/js/dicionario.js
+++ static/js/dicionario.js
@@ -1,45 +1,48 @@
-jQuery(document).ready(function () {
-   // ----------- BUSCAR NO DICIONÁRIO E EXIBIR O RESULTADO ------------------
-   jQuery('#dicionario_btn_menu').on("click", function (e) {
-      if (selected_text == '') return
-      texto = selected_text.replace(/\s+/g, ' ').trim();
-      qtde = texto.split(' ').length
-      if (qtde != 1) {
-         alert('selecione apenas uma palavra')
-         selected_text = ''
-         return;
-      }
-      jQuery.ajax({
-            url: BASE_URL + "/api_dictionary",
-            type: 'post',
-            contentType: "application/json",
-            dataType: 'json',
-            data: JSON.stringify({
-               'ra_aluno': ra_aluno,
-               'pagina': pagina_atual,
-               'palavra': selected_text,
-            }),
-            beforeSend: function () {
-               jQuery("#modal").addClass("loading");
-            }
-         })
-         .done(function (jsondata) {
-            console.log(jsondata)
-            jQuery("#modal").removeClass("loading");
-            jQuery('#dicionario_palavra').text(selected_text)
-            jQuery('#dicionario_resultado').text(jsondata)
-            jQuery('#dicionario_container').center().fadeIn(100);
-            selected_text = ''
-         })
-         .fail(function (jqXHR, textStatus, jsondata) {
-            console.log(jsondata);
-            jQuery("#modal").removeClass("loading");
-            selected_text = ''
-         })
-   })
+jQuery(document).ready(function() {
+  // ----------- BUSCAR NO DICIONÁRIO E EXIBIR O RESULTADO ------------------
+  jQuery("#dicionario_btn_menu").on("click", function(e) {
+    if (selected_text == "") return;
+    texto = selected_text.replace(/\s+/g, " ").trim();
+    qtde = texto.split(" ").length;
+    if (qtde != 1) {
+      alert("selecione apenas uma palavra");
+      selected_text = "";
+      return;
+    }
+    jQuery
+      .ajax({
+        url: BASE_URL + "/api_dictionary",
+        type: "post",
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify({
+          ra_aluno: ra_aluno,
+          pagina: pagina_atual,
+          palavra: selected_text
+        }),
+        beforeSend: function() {
+          jQuery("#modal").addClass("loading");
+        }
+      })
+      .done(function(jsondata) {
+        console.log(jsondata);
+        jQuery("#modal").removeClass("loading");
+        jQuery("#dicionario_palavra").text(selected_text);
+        jQuery("#dicionario_resultado").text(jsondata);
+        jQuery("#dicionario_container")
+          .center()
+          .fadeIn(100);
+        selected_text = "";
+      })
+      .fail(function(jqXHR, textStatus, jsondata) {
+        console.log(jsondata);
+        jQuery("#modal").removeClass("loading");
+        selected_text = "";
+      });
+  });
 
-   // ----------- FECHAR JANELA DICIONÁRIO ------------------
-   jQuery('#dicionario_btn_fechar').on('click', function () {
-      jQuery('#dicionario_container').fadeOut(100);
-   })
-})
+  // ----------- FECHAR JANELA DICIONÁRIO ------------------
+  jQuery("#dicionario_btn_fechar").on("click", function() {
+    jQuery("#dicionario_container").fadeOut(100);
+  });
+});
--- static/js/pesquisa.js
+++ static/js/pesquisa.js
@@ -1,47 +1,48 @@
-jQuery(document).ready(function () {
-   // ----------- FAZER A PESQUISA E MOSTRAR O RESULTADO ------------------
-   jQuery('#pesquisa_btn_menu').on("click", function (e) {
-      if (selected_text == '') return
-      console.log(selected_text)
-      jQuery.ajax({
-            url: BASE_URL + "/api_search",
-            type: 'post',
-            contentType: "application/json",
-            dataType: 'json',
-            data: JSON.stringify({
-               texto: selected_text,
-               ra_aluno: ra_aluno,
-               pagina: pagina_atual
-            }),
-            beforeSend: function () {
-               jQuery("#modal").addClass("loading");
-            }
-         })
-         .done(function (jsondata) {
-            console.log(jsondata)
-            response = jsondata
-            h = '';
-            for (i = 0; i < response.length; i++) {
-               h += '<a href="' + response[i].link + '"target="_blank"><span class="titulo">' + response[i].title + '</span></a><br>'
-               h += '<p class="link">' + response[i].link + '</p>'
-            }
-            jQuery("#modal").removeClass("loading");
-            jQuery('#pesquisa_conteudo').html(h);
-            jQuery('#pesquisa_container').center().fadeIn(100);
-         })
-         .fail(function (jqXHR, textStatus, jsondata) {
-            alert('Falha ao acessar o servidor');
-            jQuery("#modal").removeClass("loading");
-         })
-         selected_text = ''
-   })
+jQuery(document).ready(function() {
+  // ----------- FAZER A PESQUISA E MOSTRAR O RESULTADO ------------------
+  jQuery("#pesquisa_btn_menu").on("click", function(e) {
+    if (selected_text == "") return;
+    console.log(selected_text);
+    jQuery
+      .ajax({
+        url: BASE_URL + "/api_search",
+        type: "post",
+        contentType: "application/json",
+        dataType: "json",
+        data: JSON.stringify({
+          texto: selected_text,
+          ra_aluno: ra_aluno,
+          pagina: pagina_atual
+        }),
+        beforeSend: function() {
+          jQuery("#modal").addClass("loading");
+        }
+      })
+      .done(function(jsondata) {
+        console.log(jsondata);
+        response = jsondata;
+        h = "";
+        for (i = 0; i < response.length; i++) {
+          h += '<a href="' + response[i].link + '"target="_blank"><span class="titulo">' + response[i].title + "</span></a><br>";
+          h += '<p class="link">' + response[i].link + "</p>";
+        }
+        jQuery("#modal").removeClass("loading");
+        jQuery("#pesquisa_conteudo").html(h);
+        jQuery("#pesquisa_container")
+          .center()
+          .fadeIn(100);
+      })
+      .fail(function(jqXHR, textStatus, jsondata) {
+        alert("Falha ao acessar o servidor");
+        jQuery("#modal").removeClass("loading");
+      });
+    selected_text = "";
+  });
 
-   // ----------------- FECHAR JANELA DE PESQUISA ----------------------
-   jQuery('#pesquisa_btn_fechar').on("click", function (e) {
-      jQuery("#modal").removeClass("loading");
-      jQuery('#pesquisa_container').fadeOut(100);
-      selected_text = ''
-   })
-
-
-})
+  // ----------------- FECHAR JANELA DE PESQUISA ----------------------
+  jQuery("#pesquisa_btn_fechar").on("click", function(e) {
+    jQuery("#modal").removeClass("loading");
+    jQuery("#pesquisa_container").fadeOut(100);
+    selected_text = "";
+  });
+});
